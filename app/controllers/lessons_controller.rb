3c39926bef16799b1113756dca3ec5e7477dbb57
Muzzh <murdyn@gmail.com>
2017-07-25 15:19:57 +0000

    added verify_enrollement method in lesson controller (lesson 28)

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,10 +1,20 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :verify_enrollment
 
   def show
     
   end
 
   private
+
+  def verify_enrollment
+    if current_user.enrolled_in?(current_lesson.section.course)
+      show
+    else
+      redirect_to course_path(current_lesson.section.course), alert: 'You need to be enrolled in order to see this content'
+    end
+  end
 
   helper_method :current_lesson
   def current_lesson
