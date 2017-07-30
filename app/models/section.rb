f66f310afad301fbedc440cebe7671f55c5141fb
Muzzh <murdyn@gmail.com>
2017-07-30 14:42:52 +0000

    Added next lesson button and logic in models (lesson 37)

--- app/models/section.rb
+++ app/models/section.rb
@@ -5,6 +5,10 @@ class Section < ApplicationRecord
   validates :title, presence: true, length: { minimum: 5, maximum: 30 }
 
   include RankedModel
-
   ranks :row_order, with_same: :course_id
+
+  def next_section
+    section = course.sections.where("row_order > ?", self.row_order).rank(:row_order).first
+    return section
+  end
 end
